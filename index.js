f5e4a8d2d81521b9ebad3c5009a982e52fe4033b
Ekaterina Shmeleva <gutkati@mail.ru>
2024-12-25 15:55:45 +0400

    Создать игровое поле/Очередность хода игроков

---
+++ index.js
@@ -0,0 +1,49 @@
+let gameField = document.querySelector('#game-field')
+const winGamer = 5
+
+let numberRows = 10
+let numberColumns = 10
+
+let arrGamers = ['gamer1', 'gamer2']
+
+let arrMoveGamer1 = []
+let arrMoveGamer2 = []
+
+let numGamer = 0
+
+let currentColor = arrGamers[numGamer]
+
+
+function showGameField() {
+    getGameField(numberRows, numberColumns)
+}
+
+function getGameField(numberRows, numberColumns) {
+    for (let row = 0; row < numberRows; row++) {
+        for (let col = 0; col < numberColumns; col++) {
+            let cell = document.createElement('div')
+            cell.classList.add('cell')
+            gameField.appendChild(cell)
+            cell.addEventListener('click', handleCellClick)
+        }
+    }
+}
+
+function handleCellClick(e) {
+    let cell = e.target
+    // Ставим точку
+    const dot = document.createElement('div')
+    dot.classList.add('dot', arrGamers[numGamer])
+    cell.appendChild(dot)
+
+    // отменить клик
+    cell.removeEventListener('click', handleCellClick)
+
+    // поменять номер игрока
+    numGamer++
+    if (numGamer == arrGamers.length) {
+        numGamer = 0
+    }
+}
+
+showGameField()
