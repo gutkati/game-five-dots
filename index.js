3515f1d2de60523e7ac7c0527b12ab7bc8309cdc
Ekaterina Shmeleva <gutkati@mail.ru>
2024-12-28 09:42:24 +0400

    Функционал-определить победителя

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 let gameField = document.querySelector('#game-field')
 let colorIndicator = document.querySelector('#color-indicator span')
-const winGamer = 5
+let showWinner = document.querySelector('.end-game')
+let close = document.querySelector('.close')
+let nameGamer = document.querySelector('.name-gamer span')
+let startOver = document.querySelector('.start-over')
 
+const winGamer = 5
 let fieldSize = 9
+let numGamer = 0
+
 let arrGamers = ['gamer1', 'gamer2']
 let arrColorsGamers = ['red', 'green']
 
 let arrMoveGamer1 = []
 let arrMoveGamer2 = []
 
-let numGamer = 0
-
-const board = Array.from({length: fieldSize}, () => Array(fieldSize).fill(null));
-
-console.log('board', board)
-
 function showGameField() {
     getGameField(fieldSize)
 }
+
+startOver.addEventListener('click', function () {
+    resetGamer()
+})
 
 function getGameField(fieldSize) {
     for (let row = 0; row < fieldSize; row++) {
@@ -45,14 +49,20 @@ function handleCellClick(e) {
 
     if (arrGamers[numGamer] === 'gamer1') {
         arrMoveGamer1.push([row, col])
+        if (checkWin(arrMoveGamer1)) {
+            endGame('Красный', arrColorsGamers[numGamer])
+            gameField.classList.add('blocked') // блокируется поле
+            return
+        }
     } else {
-        arrMoveGamer2.push([row,col])
+        arrMoveGamer2.push([row, col])
+        if (checkWin(arrMoveGamer2)) {
+            endGame('Зеленый', arrColorsGamers[numGamer])
+            gameField.classList.add('blocked') // блокируется поле
+            return
+        }
     }
-
-    console.log('gamer', arrGamers[numGamer])
-    console.log('red', arrMoveGamer1)
-    console.log('green', arrMoveGamer2)
-
+    
     // поменять номер игрока
     numGamer++
     if (numGamer === arrGamers.length) {
@@ -63,9 +73,63 @@ function handleCellClick(e) {
     colorIndicator.textContent = `${arrGamers[numGamer] === 'gamer1' ? 'Красный' : 'Зеленый'}`
     colorIndicator.classList.remove('red', 'green')
     colorIndicator.classList.add(arrColorsGamers[numGamer])
-    console.log('board', board)
+}
 
+function checkWin(arrMoves) {
 
+    const directions = [
+        [0, 1], // горизонталь
+        [1, 0], // вертикаль
+        [1, 1], // диагональ вправо вниз
+        [1, -1] // диагональ вправо вверх
+    ]
+
+    for (const [x, y] of directions) {
+        for (const [res, col] of arrMoves) {
+            let count = 1
+            for (let step = 1; step <= winGamer; step++) {
+                if (arrMoves.some(([r, c]) => r === res + x * step && c === col + y * step)) {
+                    count++
+                } else {
+                    break
+                }
+            }
+
+            for (let step = 1; step <= winGamer; step++) {
+                if (arrMoves.some(([r, c]) => r === res - x * step && c === col - y * step)) {
+                    count++
+                } else {
+                    break
+                }
+            }
+
+            if (count >= winGamer) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+function endGame(name, color) {
+    showWinner.classList.add('end-game_visible')
+    nameGamer.textContent = name
+    nameGamer.classList.add(color)
+    close.addEventListener('click', function () {
+        showWinner.classList.remove('end-game_visible')
+    })
+}
+
+function resetGamer() {
+    arrMoveGamer1 = []
+    arrMoveGamer2 = []
+    numGamer = 0
+    gameField.innerHTML = '';
+    colorIndicator.classList.remove('red', 'green')
+    colorIndicator.classList.add(arrColorsGamers[numGamer])
+    colorIndicator.textContent = "Красный"
+    gameField.classList.remove('blocked')
+    showGameField()
 }
 
 showGameField()
